e6efc9aa20bfce6d7af4fce8c93ee800c2b7fd86
ffm1994 <ffm1994.1@gmail.com>
2016-03-18 22:08:29 -0300

    e
    e

--- AlgoritmoAestrela/Algoritmo/PathFinder.cs
+++ AlgoritmoAestrela/Algoritmo/PathFinder.cs
@@ -26,7 +26,6 @@ namespace Algoritmo
             InitializeNodes(searchParameters.Map);
             this.startNode = this.nodes[searchParameters.StartLocation.X, searchParameters.StartLocation.Y];
             this.startNode.State = NodeState.Open;
-            // add lista open
             this.endNode = this.nodes[searchParameters.EndLocation.X, searchParameters.EndLocation.Y];
         }
 
@@ -82,7 +81,6 @@ namespace Algoritmo
         private bool Search(Node currentNode)
         {
             // Set the current node to Closed since it cannot be traversed more than once
-            // add lista closed
             currentNode.State = NodeState.Closed;
             List<Node> nextNodes = GetAdjacentWalkableNodes(currentNode);
 
@@ -138,7 +136,6 @@ namespace Algoritmo
                 // Already-open nodes are only added to the list if their G-value is lower going via this route.
                 if (node.State == NodeState.Open)
                 {
-                    //passar custo tranversal manualmente se nao euclidiano
                     float traversalCost = Node.GetTraversalCost(node.Location, node.ParentNode.Location);
                     float gTemp = fromNode.G + traversalCost;
                     if (gTemp < node.G)
@@ -151,7 +148,6 @@ namespace Algoritmo
                 {
                     // If it's untested, set the parent and flag it as 'Open' for consideration
                     node.ParentNode = fromNode;
-                    //add lista open
                     node.State = NodeState.Open;
                     walkableNodes.Add(node);
                 }
